9a21c3730cd01d96e7e5103faf301b0311acd8d9
Nguyen Thanh Nhan <nguyenthanhnhan1014@gmail.com>
2021-10-16 02:25:48 +0700

    config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import './App.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About"
 
-function App(props) {
+function App() {
   return (
-    <div className="App">
-      <body>
-      <h1>Hello World!</h1>
-      </body>
-    </div>
+    <Router>
+      <Switch>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/HomePage">Home Page</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/HomePage">
+              <HomePage />
+            </Route>
+            <Route exact path="/About" component={About} />
+            <Route exact path="/Home" component={Home} />
+          </Switch>
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function HomePage() {
+  return <h1>Home Page</h1>
+}
+
 export default App;
